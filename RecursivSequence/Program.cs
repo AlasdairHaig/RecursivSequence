ccc5bfe57cacd1931d7ba3675e0e29094518d55c
AlasdairHaig <boaz.sylvannia@live.com>
2015-09-08 11:31:07 +0200

    ?????
    Not sure what this project does, will have to update it after I go
    through it again.

---
+++ RecursivSequence/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace RecursivSequence
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            fib(6);
+            Console.ReadLine();
+        }
+            public static int fib(int n)
+            {
+                Console.WriteLine(n);
+                if (n == 1)
+                { return 1; }
+                else if (n == 2)
+                { return 2; }
+                else
+                { return fib(n - 1) + fib(n - 2); }
+            
+            }
+
+
+
+
+
+
+    }
+}
